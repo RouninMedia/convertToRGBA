078a92c01cc2488b013265c9f6bf223121f8a8f1
Alan Lansdowne <alan_lansdowne@yahoo.com>
2022-06-30 21:20:32 +0200

    Create confirm_Input_Type.php

---
+++ confirm_Input_Type.php
@@ -0,0 +1,39 @@
+<?php
+
+function confirm_Input_Type ($input) {
+
+  $input = strtolower($input);
+
+  $input_Types = [ 'colorKeyword' => TRUE, 'hex' => TRUE ];
+  
+  $color_Keywords = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'transparent', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
+  
+  if (in_array($input, $color_Keywords)) {
+    
+    $input_Types['hex'] = FALSE;
+  }
+    
+  else {
+  
+    $input_Types['colorKeyword'] = FALSE;
+    
+    if (substr($input, 0, 1) === '#') {
+
+      $input = substr($input, 1);
+    }
+
+    if (preg_match('/[g-z]/', $input)) {
+
+      $input_Types['hex'] = FALSE;
+    }
+
+    if ((strlen($input) !== 3) && (strlen($input) !== 4) && (strlen($input) !== 6) && (strlen($input) !== 8)) {
+
+      $input_Types['hex'] = FALSE;
+    }
+  }
+
+  return $input_Types;
+}
+
+?>
